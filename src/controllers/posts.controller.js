3297768b466d5faf459d8cf71df38a7512c873df
Dhruv Patil <dhruvpatil@Dhruvs-MacBook-Air.local>
2026-02-01 22:42:48 +0530

    feat: add endpoint to get post by id

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,7 +1,7 @@
-const getAllPosts = (req, res) => {
-  res.status(200).json({
-    message: 'Route handled by postController.getAllPosts'
-  });
+const getPostById = (req, res) => {
+  const postId = req.params.postId;
+  res.status(200).json({ message: `Fetching data for post with ID: ${postId}` });
 };
 
-module.exports = { getAllPosts };
+module.exports = { getPostById };
+
